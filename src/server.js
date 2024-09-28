ad8dc8f1925d186e2bb32240ea9cecec7ea7a170
Sgedelta <Sgeaston04@gmail.com>
2024-09-28 17:58:48 -0400

    initial commit

---
+++ src/server.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const responseHandler = require('responses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+    '/': responseHandler.getIndex,
+  };
+
+
+  const onRequest = (request, response) => {
+    const protocol = request.connection.encrypted ? 'https' : 'http';
+    const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
+  
+    request.acceptedTypes = request.headers.accept.split(',');
+    request.query = Object.fromEntries(parsedUrl.searchParams);
+  
+    if (urlStruct[parsedUrl.pathname]) {
+      urlStruct[parsedUrl.pathname](request, response);
+    } else {
+      urlStruct[404](request, response);
+    }
+  };
+  
+  http.createServer(onRequest).listen(port, () => {
+    console.log(`Listening on 127.0.0.1:${port}`);
+  });
