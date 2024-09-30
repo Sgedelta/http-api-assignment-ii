a849d9347684d1950b403ab53f51bd7162496f52
Samuel Easton (RIT Student) <sge3418@rit.edu>
2024-09-30 14:59:18 -0400

    fixed eslint errors and eslint... works now

--- src/responses.js
+++ src/responses.js
@@ -6,83 +6,83 @@ const css = fs.readFileSync(`${__dirname}/../client/style.css`);
 const users = {};
 
 // generic response, not JSON specifically.
-const respond = (request, response, status, content, type ) => {
+const respond = (request, response, status, content, type) => {
+  let fixedContent = content;
+  if (type === 'application/json') {
+    fixedContent = JSON.stringify(content);
+  }
 
-    if(type === 'application/json') {
-        content = JSON.stringify(content);
-    }
+  response.writeHead(status, {
+    'Content-Type': type,
+    'Content-Length': Buffer.byteLength(fixedContent, 'utf8'),
+  });
 
-    response.writeHead(status, {
-      'Content-Type': type,
-      'Content-Length': Buffer.byteLength(content, 'utf8'),
-    });
-    
-    if (request.method !== 'HEAD' && status !== 204) {
-      response.write(content);
-    }
-  
-    response.end();
+  if (request.method !== 'HEAD' && status !== 204) {
+    response.write(fixedContent);
+  }
+
+  response.end();
 };
 
 // gets the index page
 const getIndex = (request, response) => {
-    respond(request, response, 200, index, 'text/html');
+  respond(request, response, 200, index, 'text/html');
 };
 module.exports.getIndex = getIndex;
 
 // function to get css page
 const getCSS = (request, response) => {
-    respond(request, response, 200, css, 'text/css');
-  };
+  respond(request, response, 200, css, 'text/css');
+};
 module.exports.getCSS = getCSS;
 
 const getUsers = (request, response) => {
-    const responseJSON = {
-        users
-    };
+  const responseJSON = {
+    users,
+  };
 
-    return respond(request, response, 200, responseJSON, 'application/json');
+  return respond(request, response, 200, responseJSON, 'application/json');
 };
 module.exports.getUsers = getUsers;
 
 const addUser = (request, response) => {
-    const responseJSON = {
-        message: 'Name and Age are both required',
+  const responseJSON = {
+    message: 'Name and Age are both required',
+  };
+
+  const { name, age } = request.body;
+
+  if (!name || !age) {
+    responseJSON.id = 'missingParams';
+    return respond(request, response, 400, responseJSON, 'application/json');
+  }
+
+  let status = 204;
+
+  if (!users[name]) {
+    status = 201;
+    users[name] = {
+      name,
     };
+  }
 
-    const { name, age } = request.body;
+  users[name].age = age;
 
-    if(!name || !age) {
-        responseJSON.id = 'missingParams';
-        return respond(request, response, 400, responseJSON, 'application/json');
-    }
+  if (status === 201) {
+    responseJSON.message = 'New User Created!';
+    return respond(request, response, status, responseJSON, 'application/json');
+  }
 
-    let status = 204;
-
-    if(!users[name]) {
-        status = 201;
-        users[name] = {
-            name: name,
-        };
-    }
-
-    users[name].age = age;
-
-    if(status === 201) {
-        responseJSON.message = 'New User Created!';
-        return respond(request, response, status, responseJSON, 'application/json');
-    }
-
-    return respond(request, response, status, {}, 'application/json');
+  return respond(request, response, status, {}, 'application/json');
 };
 module.exports.addUser = addUser;
 
 const pageNotFound = (request, response) => {
-    const responseJSON = {
-        message: 'Page not Found!',
-        id:'Page Not Found',
-    };
+  const responseJSON = {
+    message: 'Page not Found!',
+    id: 'Page Not Found',
+  };
 
-    return respond(request, response, 404, responseJSON, 'application/json');
-}
+  return respond(request, response, 404, responseJSON, 'application/json');
+};
 module.exports.pageNotFound = pageNotFound;
--- src/server.js
+++ src/server.js
@@ -1,64 +1,57 @@
 const http = require('http');
-const responseHandler = require('./responses.js');
 const query = require('querystring');
+const responseHandler = require('./responses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const getUrlStruct = {
-    '/': responseHandler.getIndex,
-    '/style.css': responseHandler.getCSS, 
-    '/getUsers': responseHandler.getUsers,
-    404: responseHandler.pageNotFound,
-  };
+  '/': responseHandler.getIndex,
+  '/style.css': responseHandler.getCSS,
+  '/getUsers': responseHandler.getUsers,
+  404: responseHandler.pageNotFound,
+};
 
 const postUrlStruct = {
-    '/addUser': responseHandler.addUser,
+  '/addUser': responseHandler.addUser,
 };
 
 const parseBody = (request, response, handler) => {
-    const body = [];
+  const body = [];
 
-    request.on('error', (err) => {
-        console.dir(err);
-        response.statusCode = 400;
-        response.end();
-    });
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
 
-    request.on('data', (chunk) => {
-        body.push(chunk);
-    });
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
 
-    request.on('end', () => {
-        const bodyString = Buffer.concat(body).toString();
-        request.body = query.parse(bodyString);
-        handler(request, response);
-    });
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    request.body = query.parse(bodyString);
+    handler(request, response);
+  });
 };
 
-
-
 const onRequest = (request, response) => {
-    const protocol = request.connection.encrypted ? 'https' : 'http';
-    const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
-  
-    request.query = Object.fromEntries(parsedUrl.searchParams);
+  const protocol = request.connection.encrypted ? 'https' : 'http';
+  const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
 
+  request.query = Object.fromEntries(parsedUrl.searchParams);
 
-    if(request.method === "POST") {
-        if(postUrlStruct[parsedUrl.pathname]) {
-            parseBody(request, response, responseHandler.addUser);
-        }
-    } 
-    else {
-        if (getUrlStruct[parsedUrl.pathname]) {
-            getUrlStruct[parsedUrl.pathname](request, response);
-          } else {
-            getUrlStruct[404](request, response);
-          }
+  if (request.method === 'POST') {
+    if (postUrlStruct[parsedUrl.pathname]) {
+      parseBody(request, response, responseHandler.addUser);
     }
+  } else if (getUrlStruct[parsedUrl.pathname]) {
+    getUrlStruct[parsedUrl.pathname](request, response);
+  } else {
+    getUrlStruct[404](request, response);
+  }
+};
 
-  };
-  
-  http.createServer(onRequest).listen(port, () => {
-    console.log(`Listening on 127.0.0.1:${port}`);
-  });
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1:${port}`);
+});
